78fe47981df8c203e283b8dc0928b2773d8fdd21
Gabe Guralnick <gnguralnick@gmail.com>
2022-07-02 21:53:22 -0500

    fix: update react stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import './App.scss';
+import LinkList from './components/LinkList/LinkList';
 
 function App() {
   return (
     <div className="App">
       <div>
         <p>Under construction...</p>
+        <LinkList>
+          <a href='#' onClick={e => e.preventDefault()}>I'm a Link!</a>
+        </LinkList>
       </div>
     </div>
   );
