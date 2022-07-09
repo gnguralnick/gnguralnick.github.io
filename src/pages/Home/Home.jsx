012c24952478db0cca8e6e88431a44f5bb291dc8
Gabe Guralnick <gnguralnick@gmail.com>
2022-07-09 18:16:59 -0400

    feat: initial homepage implementation

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
 
 import LinkList from '../../components/LinkList/LinkList';
+import RelativeLink from '../../components/RelativeLink/RelativeLink';
+import profile from '../../images/profile.jpeg';
+
+import Styles from './Home.module.scss';
 
 const Home = () => {
 
-    return <div className='scroll-page'>
-        <p>Under construction...</p>
-        <LinkList>
-          <a href='#' onClick={e => e.preventDefault()}>I'm a Link!</a>
-          <a href='#' onClick={e => e.preventDefault()}>I'm a Link!</a>
-        </LinkList>
-    </div>
+    return <section className={'scroll-page ' + Styles['home']} id='home'>
+        <div>
+            <h1>Gabriel Guralnick</h1>
+            <LinkList className={Styles['links']}>
+                <RelativeLink targetID='about'>about me</RelativeLink>
+                <RelativeLink targetID='projects'>my projects</RelativeLink>
+            </LinkList>
+        </div>
+        <img src={profile} alt='A picture of me' />
+        <div className={Styles['contact']}>
+
+        </div>
+    </section>
 }
 
 export default Home;
